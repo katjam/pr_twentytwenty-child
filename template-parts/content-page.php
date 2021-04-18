e92b39196a9c75878c61c01f5c6a7e56da1953b5
katjam <katjamordaunt@gmail.com>
2021-04-18 18:19:24 +0100

    Property card

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -13,30 +13,72 @@
       if ($prop_query->have_posts()) :
         while ($prop_query->have_posts()) : $prop_query->the_post();
         ?>
-        <div class="wp-block-group alignwide has-background" style="background-color: #d3d3d3">
-        <?php $status = get_post_meta( get_the_ID(), 'pr_property_listing_status', true); ?>
-          <div>
-            <div class="pr-prop-teaser-image">
+        <div class="wp-block-group alignwide property-card">
+<?php
+// detail keys: teaser, highlights, price, size, address, type
+$details = get_post_meta( get_the_ID(),'pr_property_details', true);
+$property_type = isset($details['type']) ? $details['type'] : '';
+$property_address = isset($details['address']) ? $details['address'] : '';
+$property_size = isset($details['size']) ? $details['size'] : '';
+$property_price = isset($details['price']) ? $details['price'] : '';
+$property_highlights = isset($details['highlights']) ? $details['highlights'] : '';
+$property_teaser = isset($details['teaser']) ? $details['teaser'] : '';
+$pdf = get_post_meta( get_the_ID(),'pr_property_pdf', true);
+$status = get_post_meta( get_the_ID(),'pr_property_listing_status', true);
+$disposal_type = get_post_meta( get_the_ID(),'pr_property_disposal_type', true);
+?>
+          <div class="row">
+            <div class="column">
             <?php the_post_thumbnail('medium'); ?>
             <?php if ($status && $status !== 'None'): ?>
               <span class="status"><?=$status?></span>
             <?php endif; ?>
             </div>
+            <div class="column">
+              <h3 class="type"><?= $property_type ?? the_title() ?></h3>
+              <?php if ($property_address) :?>
+              <p class="detail address"><?= nl2br($property_address) ?></p>
+              <?php endif; ?>
+            </div>
           </div>
           <div>
-            <h3><?php the_title(); ?></h3>
-            <p>
-              <?php the_excerpt(); ?>
-              <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-                See more property details
-              </a>
-            </p>
-            <?php $pdf_field = get_post_meta( get_the_ID(), 'pr_property_pdf', true); ?>
-            <?php if ($pdf_field && $pdf_field['url']) :?>
-              <a href="<?php echo $pdf_field['url'] ?>" role="button" class="button">
-                <span class="fa fa-file-pdf-o"></span>Download PDF Brochure
-              </a>
+            <?php if ($disposal_type) :?>
+            <p class="detail disposal"><?= nl2br($disposal_type) ?></p>
             <?php endif; ?>
+            <?php if ($property_size) :?>
+            <p class="detail size">
+              <span class="fa fa-arrows-alt"></span> <?= $property_size ?>
+            </p>
+            <?php endif; ?>
+            <?php if ($property_price) :?>
+            <p class="detail price">
+              <span class="fa fa-gbp"></span> <?= $property_price ?>
+            </p>
+            <?php endif; ?>
+            <?php if ($property_highlights) :?>
+            <p class="detail highlights">
+              <span class="fa fa-star"></span> <?= $property_highlights ?>
+            </p>
+            <?php endif; ?>
+            <?php if ($property_teaser) :?>
+            <p class="detail teaser">
+              <span class="fa fa-check"></span> <?= $property_teaser ?>
+            </p>
+            <?php endif; ?>
+            <div class="row">
+              <div class="column">
+                <a href="<?php the_permalink(); ?>" class="button">
+                See more property details <span class="fa fa-arrow-right"></span>
+                </a>
+              </div>
+              <?php if ($pdf && $pdf['url']) :?>
+              <div class="column">
+                <a href="<?php echo $pdf['url'] ?>" class="button">
+                  Download PDF Brochure <span class="fa fa-file-pdf-o"></span>
+                  </a>
+              </div>
+            <?php endif; ?>
+            </div>
           </div>
         </div>
 
