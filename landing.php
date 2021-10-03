e41100d28057e52381c98cd42f9efa34b0ae8384
katjam <katjamordaunt@gmail.com>
2021-10-03 16:02:36 +0100

    Add 3 images template parts

---
+++ landing.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying landing page.
+ */
+
+get_header();
+?>
+
+<main id="site-content" role="main">
+
+	<?php
+
+	if ( have_posts() ) {
+
+		while ( have_posts() ) {
+			the_post();
+
+			get_template_part( 'template-parts/content-three-image', get_post_type() );
+		}
+	}
+
+	?>
+
+</main><!-- #site-content -->
+
+<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+
+<?php get_footer(); ?>
---
+++ template-parts/content-three-image.php
@@ -0,0 +1,8 @@
+<div class="entry-content page-content landing-page">
+  <div class="post-inner alignwide">
+    <?php
+        the_content();
+        edit_post_link();
+    ?>
+  </div>
+</div>
